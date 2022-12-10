11c6c0e918a197ae5c64493bcf462a8854de9198
AprilJoyW <46820149+AprilJoyW@users.noreply.github.com>
2022-12-09 20:18:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -290,9 +290,10 @@ age = st.slider("Age",
                 value=50)
 st.write(f"Age: {age}")
 #"marital","age", "gender","par","educ2","income"]
+
 marital= pandas.to_numeric(marital)
 age= pandas.to_numeric(age)
-gender= pandas.to_numeric(gender)
+#gender= pandas.to_numeric(gender)
 par= pandas.to_numeric(par)
 educ2= pandas.to_numeric(educ2)
 incomenumber= pandas.to_numeric(incomenumber)
